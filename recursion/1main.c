60235e97531b257426aedad52e3ac8174483c5e8
Alessandro S <32938695+asaggse@users.noreply.github.com>
2020-04-22 19:58:31 +0200

    Create 1main.c
    //Write a recursive function in C to print all natural numbers from 1 to 50

---
+++ recursion/1main.c
@@ -0,0 +1,21 @@
+//Write a recursive function in C to print all natural numbers from 1 to 50
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void recursive_print(int);
+
+int main(){
+  int num = 1;
+  
+  recursive_print(num);
+  
+  return EXIT_SUCCESS;
+}
+
+void recursive_print(int n){
+  if(n <= 50){
+    printf("%d", n);
+    recursive_print(n+1);
+  }
+}
